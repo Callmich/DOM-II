819c1345fbdb027ef652f03b53fef50318c8fe87
Michael Callahan <CallahanMichaelE@gmail.com>
2020-02-11 13:31:57 -0700

    mouseover & mouse leave used on the nav bar

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
 // Your code goes here
+
+
+document.querySelectorAll('nav a').forEach(el => {
+    el.addEventListener('mouseenter', () => {
+        el.style.transform = 'scale(1.5)';
+        el.style.transition ='transform .5s';
+        el.style.color = "red";
+    })
+})
+
+document.querySelectorAll('nav a').forEach(el => {
+    el.addEventListener('mouseleave', () => {
+        el.style.transform = 'scale(1)';
+        el.style.transition ='transform .5s';
+        el.style.color = "black";
+    })
+})
